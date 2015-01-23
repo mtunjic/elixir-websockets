b0bae8ed32800dcbd2c327004fce43a48e2265f6
Marko Tunjic <mtunjic@MTU-RAZOR.local>
2015-01-23 16:59:17 +0100

    ws handler

---
+++ lib/ws_handler.ex
@@ -0,0 +1,38 @@
+defmodule ElixirWs.WsHandler do
+  def init(_, _, _) do
+    {:upgrade, :protocol, :cowboy_websocket}
+  end
+  
+  
+  def websocket_init(_, req, _) do
+    send_messages(self)
+    {:ok, req, nil}
+  end
+  
+  defp send_messages(process) do
+    :panding
+  end
+  
+  
+  def websocket_info({:message, msg}, req, state) do
+    {:reply, {:text, msg}, req, state}
+  end
+  
+  def websocket_info(_, req, state) do
+    {:ok, req, state}
+  end
+  
+  
+  def websocket_handle({:text, msg}, req, state) do
+    {:reply, {:text, "#{msg}"}, req, state}
+  end
+  
+  def websocket_handle(_, req, state) do
+    {:ok, req, state}
+  end
+  
+  
+  def websocket_terminate(_, _, _) do
+    :ok
+  end
+end
